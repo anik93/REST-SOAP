27c96b3bf6828f1217271dc6193ea631b88df2b8
anik93 <proxianik@gmail.com>
2016-04-20 11:44:34 +0200

    final

--- client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
+++ client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
@@ -2,11 +2,17 @@ package wdsr.exercise3.hr;
 
 import java.net.URL;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
-import javax.xml.ws.WebServiceClient;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.ws.soap.SOAPFaultException;
 
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-
+import wdsr.exercise3.ws.EmployeeType;
+import wdsr.exercise3.ws.HolidayRequest;
+import wdsr.exercise3.ws.HolidayResponse;
+import wdsr.exercise3.ws.HolidayType;
 import wdsr.exercise3.ws.HumanResource;
 import wdsr.exercise3.ws.HumanResourceService;
 
@@ -14,20 +20,16 @@ import wdsr.exercise3.ws.HumanResourceService;
 // In order to see definition of the Human Resource web service:
 // 1. Run HolidayServerApp.
 // 2. Go to http://localhost:8090/holidayService/?wsdl
-@WebServiceClient(name = "HolidayServerApp", 
-				targetNamespace = "http://localhost:8090/holidayService", 
-				wsdlLocation = "http://localhost:8090/holidayService/?wsdl")
 public class HolidayClient {
 	
-	private HumanResourceService hrs;
-	//JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+	private HumanResource client;
 	/**
 	 * Creates this object
 	 * @param wsdlLocation URL of the Human Resource web service WSDL
 	 */
 	public HolidayClient(URL wsdlLocation) {
-		hrs = new HumanResourceService(wsdlLocation);
-		//factory.create();
+		HumanResourceService hrs = new HumanResourceService(wsdlLocation);
+		client = hrs.getHumanResourcePort();
 	}
 	
 	/**
@@ -41,7 +43,33 @@ public class HolidayClient {
 	 * @throws ProcessingException if request processing fails.
 	 */
 	public int bookHoliday(int employeeId, String firstName, String lastName, Date startDate, Date endDate) throws ProcessingException {
-		// TODO
-		return 0;
+		HolidayRequest holidayRequest = new HolidayRequest();
+		EmployeeType employeeType = new EmployeeType();
+		employeeType.setNumber(employeeId);
+		employeeType.setFirstName(firstName);
+		employeeType.setLastName(lastName);
+		holidayRequest.setEmployee(employeeType);
+		
+		HolidayType holidayType = new HolidayType();
+		GregorianCalendar c = new GregorianCalendar();
+		c.setTime(startDate);
+		XMLGregorianCalendar date1 = null;
+		try {
+			date1 = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+			holidayType.setStartDate(date1);
+			c.setTime(endDate);
+			date1 = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+			holidayType.setEndDate(date1);
+		} catch (DatatypeConfigurationException e) {
+			e.printStackTrace();
+		}
+		
+		holidayRequest.setHoliday(holidayType);
+		
+		try{
+			return client.holiday(holidayRequest).getRequestId();
+		} catch (SOAPFaultException e){
+			throw new ProcessingException();
+		}
 	}
 }
